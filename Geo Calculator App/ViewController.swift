a4ec8c54deafdb61833d08e078e88001fc60eac1
student <student@students-imac-3.local>
2018-05-15 21:23:13 -0400

    implemented logic for calculate and clear

--- Geo Calculator App/ViewController.swift
+++ Geo Calculator App/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
@@ -34,9 +35,38 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+        // used to check for valid points
+        var pointsOk: Bool = false
+        
+        // if optionals not nil
+        if let p1Lat = self.p1LatField.text, let p1Long = self.p1LongField.text, let p2Lat = self.p2LatField.text, let p2Long = self.p2LongField.text {
+            
+            // if text fields not empty strings
+            if p1Lat != "", p1Long != "", p2Lat != "", p2Long != "" {
+                
+                // all points valid
+                pointsOk = true
+            }
+        }
+        
+        // if all points valid
+        if pointsOk {
+            
+            // create points from latitudes and longitudes
+            var p1: CLLocation = CLLocation(latitude: Double(self.p1LatField.text!)!, longitude: Double(self.p1LongField.text!)!)
+            var p2: CLLocation = CLLocation(latitude: Double(self.p2LatField.text!)!, longitude: Double(self.p2LongField.text!)!)
+            
+            // calculate distance between p1 and p2
+            
+        }
     }
     
     @IBAction func clearButtonPressed(_ sender: UIButton) {
+        p1LatField.text = ""
+        p2LatField.text = ""
+        p1LongField.text = ""
+        p2LongField.text = ""
     }
 }
 
