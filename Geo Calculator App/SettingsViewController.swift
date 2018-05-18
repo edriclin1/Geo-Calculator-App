0f71ba11ed12c4105271e623ef72ce60d8153de6
student <student@students-imac-2.local>
2018-05-17 21:34:37 -0400

    added picker to settings

--- Geo Calculator App/SettingsViewController.swift
+++ Geo Calculator App/SettingsViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
 
+    @IBOutlet weak var distanceUnitsLabel: UILabel!
+    @IBOutlet weak var bearingUnitsLabel: UILabel!
+    @IBOutlet weak var unitsPickerView: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    var selection : String = "Steak"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // open uipickerview for distance when distance label clicked
+        let detectDistanceTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.showDistanceUnitsPickerView))
+        self.distanceUnitsLabel.addGestureRecognizer(detectDistanceTouch)
+        
+        // open uipickerview for bearing when bearing label clicked
+        let detectBearingTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.showBearingUnitsPickerView))
+        self.bearingUnitsLabel.addGestureRecognizer(detectBearingTouch)
+        
+        self.unitsPickerView.delegate = self
+        self.unitsPickerView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +40,16 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func showDistanceUnitsPickerView() {
+        self.pickerData = ["Kilometers", "Miles"]
+        self.unitsPickerView.isHidden = false
+    }
 
+    @objc func showBearingUnitsPickerView() {
+        self.pickerData = ["Degrees", "Mils"]
+        self.unitsPickerView.isHidden = false
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -38,5 +66,26 @@ class SettingsViewController: UIViewController {
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfComponents(in: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return self.pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        self.selection = self.pickerData[row]
     }
 }
